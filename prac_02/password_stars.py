fc93a4afc86007ee8ad72bb2ffe111160f839e2a
phuongnam2709 <phuongnam.nguyen2@my.jcu.edu.au>
2024-10-26 14:57:41 +0800

    dd password check program

---
+++ prac_02/password_stars.py
@@ -0,0 +1,9 @@
+MIN_LENGTH = 8
+
+password = input("Enter a password: ")
+
+while len(password) < MIN_LENGTH:
+    print(f"Password must be at least {MIN_LENGTH} characters long.")
+    password = input("Enter a password: ")
+
+print("*" * len(password))
